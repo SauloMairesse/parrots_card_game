f6a6bca998e6ccc33ee24e50c9b1a8164b4f557c
Saulo Mairesse <saulodavi2001@gmail.com>
2022-02-05 01:23:06 -0300

    VERSÃO ALPHA, FUNCIONALIDADES BASICAS OKAY, FALTANDO POLIMENTO E CORREÇÃO DE BUGS

--- script.js
+++ script.js
@@ -7,40 +7,70 @@ function comparador() {
 imagem.sort(comparador); //Imagens embaralhadas.
 let mainHTML = document.querySelector('main');
 let asCartas = [];
+let primeiraCarta;
+let segundaCarta;
 
 // Preparando HTML PARA MOSTRAR O N CERTO DE CARTAS
+
 for (let i = 0; i < nCartas; i++) {
     // novaCarta.document.querySelector(".costas img")).setAttribute('src', `${imagem[i]}`)
     let novaCarta = `<div class="espaçoDaCarta">                          
                         <div class="carta" onclick="virarCarta(this)">
                             <div class="frente">
                                 <img src="arquivosUteis/front.png" alt="">
+                            </div>
+                            <div class="costas">
+                                <img src="${imagem[i]}" alt="">
+                            </div>
                         </div>
-                        <div class="costas">
-                            <img src="${imagem[i]}" alt="">
-                        </div>
-                    </div>
-                </div>`;
+                    </div>`;
+    console.log(asCartas)
     asCartas.push(novaCarta);
     asCartas.push(novaCarta);
     asCartas.sort(comparador);
 }
 // "Postando" As IMAGENS NO HTML
+
 for(let i = 0; i < asCartas.length; i++){
     mainHTML.innerHTML = mainHTML.innerHTML + asCartas[i];
 }
 
 //  ------- COMEÇO DO GAME --------
+
 function virarCarta(diviSelecionada){
-    diviSelecionada.classList.add('viradinha');
-    diviSelecionada.classList.add('escolhida');
-    anem();
-}
-function anem(){
-    let cartasEscolhidas = document.querySelectorAll('.escolhida .costas img');
-    if(cartasEscolhidas[0].innerHTML !== cartasEscolhidas[1].innerHTML){
-        document.querySelector('.escolhida').classList.remove('viradinha');
-        document.querySelector('.escolhida').classList.remove('escolhida');
+        diviSelecionada.classList.add('viradinha');
+        if(!document.querySelector('.primeiraCarta')){
+            diviSelecionada.classList.add('primeiraCarta');
+            primeiraCarta = diviSelecionada;
+            primeiraCarta.setAttribute('onclick','')
+            console.log(primeiraCarta);
+            return false;
         }
-    console.log(cartasEscolhidas);
+            diviSelecionada.classList.add('segundaCarta');
+            segundaCarta = diviSelecionada;
+            console.log(segundaCarta);
+            setTimeout(compararCartas,1000);
+            console.log(document.querySelectorAll('.estouComMeuPar').length);
+            console.log(asCartas.length);
 }
+
+function compararCartas(){
+    if(primeiraCarta.innerHTML !== segundaCarta.innerHTML){
+        primeiraCarta.classList.remove('primeiraCarta');
+        primeiraCarta.classList.remove('viradinha');
+        segundaCarta.classList.remove('segundaCarta');
+        segundaCarta.classList.remove('viradinha');
+        primeiraCarta.setAttribute('onclick','virarCarta(this)');
+    }
+    else{
+        primeiraCarta.classList.remove('primeiraCarta');
+        primeiraCarta.classList.add('estouComMeuPar');
+        primeiraCarta.setAttribute('onclick','');
+        segundaCarta.classList.remove('segundaCarta');
+        segundaCarta.setAttribute('onclick','');
+        segundaCarta.classList.add('estouComMeuPar');
+    }
+}
+// if(document.querySelectorAll('.estouComMeuPar').length === asCartas.length){
+//     alert('VOCE GANHOUUUU');
+// }
