60460e336b73ea7b826f893a3b64fe43ebf32f67
Saulo Mairesse <saulodavi2001@gmail.com>
2022-02-06 16:57:38 -0300

    Colocando Condições para iniciar o jogo

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
 // Declarando e Preparando Variáveis Globais
-const nCartas = parseInt(prompt('Entre 2 e 7 pares, Quantos Pares de carta deseja?'));
-const imagem = ["arquivosUteis/bobrossparrot.gif","arquivosUteis/explodyparrot.gif","arquivosUteis/fiestaparrot.gif","arquivosUteis/metalparrot.gif","arquivosUteis/revertitparrot.gif","arquivosUteis/tripletsparrot.gif","arquivosUteis/unicornparrot.gif",];
-function comparador() { 
-	return Math.random() - 0.5;
-}
-imagem.sort(comparador); //Imagens embaralhadas.
+
 let mainHTML = document.querySelector('main');
 let asCartas = [];
 let primeiraCarta;
 let segundaCarta;
+const imagem = ["arquivosUteis/bobrossparrot.gif","arquivosUteis/explodyparrot.gif","arquivosUteis/fiestaparrot.gif","arquivosUteis/metalparrot.gif","arquivosUteis/revertitparrot.gif","arquivosUteis/tripletsparrot.gif","arquivosUteis/unicornparrot.gif",];
+var nCartas;
+
+iniciarJogo()
 
 // Preparando HTML PARA MOSTRAR O N CERTO DE CARTAS
 
-for (let i = 0; i < nCartas; i++) {
-    // novaCarta.document.querySelector(".costas img")).setAttribute('src', `${imagem[i]}`)
-    let novaCarta = `<div class="espaçoDaCarta">                          
-                        <div class="carta" onclick="virarCarta(this)">
-                            <div class="frente">
-                                <img src="arquivosUteis/front.png" alt="">
-                            </div>
-                            <div class="costas">
-                                <img src="${imagem[i]}" alt="">
-                            </div>
-                        </div>
-                    </div>`;
-    console.log(asCartas)
-    asCartas.push(novaCarta);
-    asCartas.push(novaCarta);
-    asCartas.sort(comparador);
-}
-// "Postando" As IMAGENS NO HTML
-
-for(let i = 0; i < asCartas.length; i++){
-    mainHTML.innerHTML = mainHTML.innerHTML + asCartas[i];
+function iniciarJogo(){
+    imagem.sort(comparador); //Imagens embaralhadas.
+    while(true){
+        nCartas = parseInt(prompt('Entre 2 e 7 pares, Quantos Pares de carta deseja?'));
+        if(nCartas >= 2 && nCartas <= 7){
+            cartasNaMesa();
+            return false;
+        }
+        else {
+            alert('Respeite o Limite de Cartas');
+        }
+    }
 }
 
-//  ------- COMEÇO DO GAME --------
+function cartasNaMesa(){
+    for (let i = 0; i < nCartas; i++) {
+        let novaCarta = `<div class="espaçoDaCarta">                          
+                            <div class="carta" onclick="virarCarta(this)">
+                                <div class="frente">
+                                    <img src="arquivosUteis/front.png" alt="">
+                                </div>
+                                <div class="costas">
+                                    <img src="${imagem[i]}" alt="">
+                                </div>
+                            </div>
+                        </div>`;
+        console.log(asCartas)
+        asCartas.push(novaCarta);
+        asCartas.push(novaCarta);
+        asCartas.sort(comparador);
+    }
+    // "Postando" As IMAGENS NO HTML
+    for(let i = 0; i < asCartas.length; i++){
+        mainHTML.innerHTML = mainHTML.innerHTML + asCartas[i];
+    }
+}
+
+//  ------- Funções --------
 
 function virarCarta(diviSelecionada){
         diviSelecionada.classList.add('viradinha');
@@ -71,6 +84,7 @@ function compararCartas(){
         segundaCarta.classList.add('estouComMeuPar');
     }
 }
-// if(document.querySelectorAll('.estouComMeuPar').length === asCartas.length){
-//     alert('VOCE GANHOUUUU');
-// }
+
+function comparador() { 
+	return Math.random() - 0.5;
+}
